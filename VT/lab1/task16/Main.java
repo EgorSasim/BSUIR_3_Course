f689ac0ee73f74ce2d50fce0adbd2f12c15f67aa
egor.sasim <egor.sasim@altezza.org>
2022-09-11 14:52:34 +0300

    create title and author comparator, VT-1

--- VT/lab1/task16/Main.java
+++ VT/lab1/task16/Main.java
@@ -4,21 +4,21 @@ import java.util.Collections;
 class Main {
     public static void main(String[] args) {
         ArrayList<Book> arr = new ArrayList<Book>();
-        arr.add(new Book("book1", "author1", 1));
+        arr.add(new Book("book1", "author2", 1));
         arr.add(new Book("book7", "author1", 1));
-        arr.add(new Book("book2", "author1", 1));
+        arr.add(new Book("book1", "author1", 1));
         arr.add(new Book("book6", "author1", 1));
         arr.add(new Book("book3", "author1", 1));
 
         for (int i = 0; i < arr.size(); ++i) {
-            System.out.println(arr.get(i).title);
+            System.out.println(arr.get(i).title + " " + arr.get(i).author);
         }
         System.out.println();
 
-        Collections.sort(arr, new sortByTitle());
+        Collections.sort(arr, new sortByTitleAuthor());
 
         for (int i = 0; i < arr.size(); ++i) {
-            System.out.println(arr.get(i).title);
+            System.out.println(arr.get(i).title  + " " + arr.get(i).author);
         }
         System.out.println();
     }
