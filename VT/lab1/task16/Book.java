2ad6c8f09466388856975eaa446d20077e2e8b3c
egor.sasim <egor.sasim@altezza.org>
2022-09-11 14:40:52 +0300

    create sep file for comparator class, VT-1

--- VT/lab1/task16/Book.java
+++ VT/lab1/task16/Book.java
@@ -44,8 +44,3 @@ class Book {
 
 }
 
-class sortByTitle implements Comparator<Book> {
-	public int compare(Book book1, Book book2) {
-		return book1.title.compareTo(book2.title);
-	} 
-}
--- VT/lab1/task16/Main.java
+++ VT/lab1/task16/Main.java
@@ -1,6 +1,5 @@
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 
 class Main {
     public static void main(String[] args) {
