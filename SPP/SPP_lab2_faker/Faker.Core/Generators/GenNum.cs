8e239b4693ed94584c78a957e6bb60c59c19a2cb
egor.sasim <egor.sasim@altezza.org>
2022-09-18 11:23:31 +0300

    create numeric generator, SPP-2

---
+++ SPP/SPP_lab2_faker/Faker.Core/Generators/GenNum.cs
@@ -0,0 +1,22 @@
+using System.Reflection;
+namespace Faker.Core.Generators 
+{
+    internal class GeneratorNumeric: Generator 
+    {
+         static GeneratorNumeric()
+        {
+            var genFunctions = typeof(GeneratorNumeric).GetMethods(BindingFlags.NonPublic | BindingFlags.Static);
+
+            foreach (var func in genFunctions) simpleGenerators[func.ReturnType] = () => func.Invoke(null, null);
+        }
+
+        private static short Int16() => BitConverter.ToInt16(GetBytes(sizeof(short)));
+        private static int Int32() => BitConverter.ToInt32(GetBytes(sizeof(int)));
+        private static long Int64() => BitConverter.ToInt64(GetBytes(sizeof(long)));
+        private static ushort UInt16() => BitConverter.ToUInt16(GetBytes(sizeof(ushort)));
+        private static uint UInt32() => BitConverter.ToUInt32(GetBytes(sizeof(uint)));
+        private static ulong UInt64() => BitConverter.ToUInt64(GetBytes(sizeof(ulong)));
+        private static float Single() => BitConverter.ToSingle(GetBytes(sizeof(float)));
+        private static double Double() => BitConverter.ToDouble(GetBytes(sizeof(double)));
+    }
+}
