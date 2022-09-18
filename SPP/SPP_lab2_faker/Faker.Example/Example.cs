bb9d8c0bd2fcbc825fa52ab755cb2745f879f79b
egor.sasim <egor.sasim@altezza.org>
2022-09-18 12:01:01 +0300

    remove unnecessary usings, SPP-2

--- SPP/SPP_lab2_faker/Faker.Example/Example.cs
+++ SPP/SPP_lab2_faker/Faker.Example/Example.cs
@@ -1,25 +1,26 @@
 ﻿using Faker.Core;
-using Faker.Core.Interfaces;
-using Faker;
 
-class Example 
+namespace Faker.Example 
 {
-    public static void Main() 
-    {
-        int randInt = 0;
-        float randFloat = 0.0F;
-        double randDouble = 0.0;
-        string randString = "";
-        string randString2 = "";
+    class Example 
+    {   
+        public static void Main() 
+        {
+            int randInt = 0;
+            float randFloat = 0.0F;
+            double randDouble = 0.0;
+            string randString = "";
+            string randString2 = "";
 
-        Faker23 _faker = new Faker23();
+            Faker23 _faker = new Faker23();
 
-        randInt = _faker.Create<int>();
-        randFloat = _faker.Create<float>();
-        randDouble = _faker.Create<double>();
-        randString = _faker.Create<string>();
-        randString2 = _faker.Create<string>();
+            randInt = _faker.Create<int>();
+            randFloat = _faker.Create<float>();
+            randDouble = _faker.Create<double>();
+            randString = _faker.Create<string>();
+            randString2 = _faker.Create<string>();
 
-        Console.WriteLine($"Rand int: {randInt}\nRand float: {randFloat}\nRand double: {randDouble}\nRand string: {randString}\nRand string2: {randString2}");
+            Console.WriteLine($"Rand int: {randInt}\nRand float: {randFloat}\nRand double: {randDouble}\nRand string: {randString}\nRand string2: {randString2}");
+        }
     }
 }
