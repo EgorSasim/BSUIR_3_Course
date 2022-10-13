c4a24e1a79354ecb900eb79be379fdd905981209
egor.sasim <egor.sasim@altezza.org>
2022-10-13 16:39:56 +0300

    add repetition amount, CRW

--- CourseWork/cntrep.cpp
+++ CourseWork/cntrep.cpp
@@ -1,26 +1,58 @@
 #include <iostream>
 #include <stdlib.h>
+#include <ctype.h>
 
 int *REPETITION_COUNTING_ARRAY;
 int REPETITION_COUNTING_ARRAY_LENGTH;
+int REPETITION_AMOUNT;
+int UNIQUE_RANDOM_VALUES_ARRAY;
+int UNIQUE_RANDOM_VALUES_ARRAY_LENGTH;
 
+
+int* initRepetitionCountingArray(int arrayLength);
 int fillRepetitionCountingArray(int* repetitionCountingArray, int arrayLength);
 int showRepetitionCountingArray(int* repetitionCountingArray, int arrayLength);
+bool isNumber(char number[]);
 
 int main(int argc, char *argv[])
 {
 	srand(time(NULL));
-	if (!argv[1])
+	if (argc < 3)
 	{	
-		printf("Can't find counting array length\n");
+		printf("Not enough parameters. Should be at least 2:\n1. Array size\n2. Repetition amount\n");
+		return -1;
+	}
+	if (!isNumber(argv[1]) || !isNumber(argv[2]))
+	{
+		printf("Incorrect parameters values(you should pass two numbers)\n");
+		return -1;
+	}
+	
+	REPETITION_COUNTING_ARRAY_LENGTH = atoi(argv[1]);
+	REPETITION_AMOUNT = atoi(argv[2]);
+
+	if (REPETITION_COUNTING_ARRAY_LENGTH <= 0 || REPETITION_AMOUNT <= 0)
+	{
+		printf("Arguments values should be more than zero\n");
+		return -1;
+	}
+	if (REPETITION_COUNTING_ARRAY_LENGTH % REPETITION_AMOUNT != 0) 
+	{
+		printf("Array length should be divided by repetitions amount without rest\n");
 		return -1;
 	}
 
-	REPETITION_COUNTING_ARRAY_LENGTH = atoi(argv[1]);
-	REPETITION_COUNTING_ARRAY = (int *)calloc(REPETITION_COUNTING_ARRAY_LENGTH, sizeof (int));
+
+	REPETITION_COUNTING_ARRAY = initRepetitionCountingArray(REPETITION_COUNTING_ARRAY_LENGTH);
 	fillRepetitionCountingArray(REPETITION_COUNTING_ARRAY, REPETITION_COUNTING_ARRAY_LENGTH);
 	showRepetitionCountingArray(REPETITION_COUNTING_ARRAY, REPETITION_COUNTING_ARRAY_LENGTH);
+	free(REPETITION_COUNTING_ARRAY);
 	return 0;
+}
+
+int* initRepetitionCountingArray(int arrayLength)
+{
+	return (int *)calloc(arrayLength, sizeof (int));
 }
 
 int fillRepetitionCountingArray(int* repetitionCountingArray, int arrayLength)
@@ -42,3 +74,17 @@ int showRepetitionCountingArray(int* repetitionCountingArray, int arrayLength)
 	printf("that's all\n");
 	return 0;
 }
+
+bool isNumber(char number[])
+{
+	int i = 0;
+	if (number[0] == '-')
+		i = 1;
+	for (; number[i] != 0; ++i)
+	{
+		if (!isdigit(number[i]))
+			return 0;
+	}
+	return 1;
+}
+
