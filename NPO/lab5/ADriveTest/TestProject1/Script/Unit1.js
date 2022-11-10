93c5e72ac5173f2c95d96a9ae9e68f1e923b0f83
briarey <egorsasim@mail.ru>
2022-11-10 05:59:27 -0800

    pass lab, NPO-5

---
+++ NPO/lab5/ADriveTest/TestProject1/Script/Unit1.js
@@ -0,0 +1,237 @@
+﻿function MainTest()
+{
+  Test1();
+  Test2();
+  Test3();
+  Test4();
+  Test5();
+  Test6();  
+}
+
+function Test1()
+{
+  TestedApps.PDDTests.Run();
+  //Clicks the 'DesktopWindowXamlSource' object.
+  Aliases.explorer.wndShell_TrayWnd.DesktopWindowXamlSource.Click(1188, 49);
+  //Clicks the 'AmButton' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.AmButton.Click(78, 18);
+  //Clicks the 'AmButton3' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.AmButton3.Click(57, 3);
+  //Clicks the 'Button2' button.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.ItemsControl.Button2.ClickButton();
+  //Clicks the 'AmButton2' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.AmButton2.Click(45, 30);
+  TestedApps.PDDTests.Close();
+}
+
+function Test2()
+{
+  TestedApps.PDDTests.Run();
+  //Clicks the 'AmButton' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.AmButton.Click(70, 6);
+  //Clicks the 'AmButton3' object.
+  //Aliases.PDDTests.HwndSource_MainWindow.MainWindow.AmButton3.Click(85, 14);
+  Sys.Process("PDDTests").WPFObject("HwndSource: MainWindow", "ADrive ????? 2022 ?? ???????? ????????? ????????").WPFObject("MainWindow", "ADrive ????? 2022 ?? ???????? ????????? ????????", 1).WPFObject("SubjectMenuView", "", 1).WPFObject("Grid", "", 1).WPFObject("Grid", "", 3).WPFObject("DockPanel", "", 1).WPFObject("ItemsControl", "", 1).WPFObject("ContentPresenter", "", 5).Click();
+  //Clicks the 'Button2' button.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.ItemsControl.Button2.ClickButton();
+  //Clicks the 'AmButton2' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.AmButton2.Click(94, 25);
+  //Enters '2' in the 'MainWindow' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys("2");
+  //Enters '[Enter]' in the 'MainWindow' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys("[Enter]");
+  //Enters '[Enter]2' in the 'MainWindow' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys("[Enter]2");
+  //Enters '1' in the 'MainWindow' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys("1");
+  //Enters '[Enter]' in the 'MainWindow' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys("[Enter]");
+  //Enters '[Enter]1' in the 'MainWindow' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys("[Enter]1");
+  //Enters '[Enter]' in the 'MainWindow' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys("[Enter]");
+  //Enters '[Enter]2' in the 'MainWindow' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys("[Enter]2");
+  //Enters '[Enter]' in the 'MainWindow' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys("[Enter]");
+  //Enters '[Enter]2' in the 'MainWindow' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys("[Enter]2");
+  //Enters '[Enter]' in the 'MainWindow' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys("[Enter]");
+  //Enters '[Enter]2' in the 'MainWindow' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys("[Enter]2");
+  //Enters '[Enter]' in the 'MainWindow' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys("[Enter]");
+  //Enters '[Enter]2' in the 'MainWindow' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys("[Enter]2");
+  //Enters '[Enter]' in the 'MainWindow' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys("[Enter]");
+  //Enters '[Enter]2' in the 'MainWindow' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys("[Enter]2");
+  //Enters '[Enter]' in the 'MainWindow' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys("[Enter]");
+  //Enters '[Enter]4' in the 'MainWindow' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys("[Enter]4");
+  //Enters '[Enter]' in the 'MainWindow' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys("[Enter]");
+  //Enters '[Enter]2' in the 'MainWindow' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys("[Enter]2");
+  //Enters '[Enter]' in the 'MainWindow' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys("[Enter]");
+  //Enters '[Enter][Esc]' in the 'MainWindow' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys("[Enter][Esc]");
+  TestedApps.PDDTests.Close();
+}
+
+function Test3()
+{
+  TestedApps.PDDTests.Run();
+  //Clicks the 'AmButton3' object.
+  Sys.Process("PDDTests").WPFObject("HwndSource: MainWindow", "ADrive ????? 2022 ?? ???????? ????????? ????????").WPFObject("MainWindow", "ADrive ????? 2022 ?? ???????? ????????? ????????", 1).WPFObject("SubjectMenuView", "", 1).WPFObject("Grid", "", 1).WPFObject("StackPanel", "", 1).WPFObject("StackPanel", "", 1).WPFObject("Border", "", 3).Click();
+  //Clicks the 'Button' button.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.ItemsControl.Button.ClickButton();
+  //Clicks the 'AmButton2' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.AmButton2.Click(83, 21);
+  //Enters '2' in the 'MainWindow' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys("2");
+  //Enters '[Enter]' in the 'MainWindow' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys("[Enter]");
+  //Enters '[Enter]4' in the 'MainWindow' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys("[Enter]4");
+  //Enters '[Enter]' in the 'MainWindow' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys("[Enter]");
+  //Enters '[Enter]2' in the 'MainWindow' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys("[Enter]2");
+  //Enters '[Enter]' in the 'MainWindow' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys("[Enter]");
+  //Enters '[Enter]2' in the 'MainWindow' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys("[Enter]2");
+  //Enters '[Enter]' in the 'MainWindow' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys("[Enter]");
+  //Enters '[Enter]2' in the 'MainWindow' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys("[Enter]2");
+  //Enters '[Enter]' in the 'MainWindow' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys("[Enter]");
+  //Enters '[Enter]2' in the 'MainWindow' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys("[Enter]2");
+  //Enters '1' in the 'MainWindow' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys("1");
+  //Enters '[Enter]' in the 'MainWindow' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys("[Enter]");
+  //Enters '[Enter]2' in the 'MainWindow' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys("[Enter]2");
+  //Enters '[Enter]' in the 'MainWindow' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys("[Enter]");
+  //Enters '[Enter]2' in the 'MainWindow' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys("[Enter]2");
+  //Enters '3' in the 'MainWindow' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys("3");
+  //Enters '[Enter]' in the 'MainWindow' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys("[Enter]");
+  //Enters '[Enter]2' in the 'MainWindow' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys("[Enter]2");
+  //Enters '[Enter]' in the 'MainWindow' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys("[Enter]");
+  //Enters '[Enter]2' in the 'MainWindow' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys("[Enter]2");
+  //Enters '[Enter]' in the 'MainWindow' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys("[Enter]");
+  //Enters '[Enter]' in the 'MainWindow' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys("[Enter]");
+  //Clicks the 'Textbutton' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Textbutton.Click(102, 20);
+  //Enters '[Esc][Esc]' in the 'MainWindow' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys("[Esc][Esc]");
+  TestedApps.PDDTests.Close();
+}
+
+function Test4()
+{
+  if (Sys.Browser("chrome").IsOpen) Sys.Browser("chrome").Terminate();
+  TestedApps.PDDTests.Run();
+  //Clicks the 'HyperlinkAdriveSmart27102022' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.HyperlinkAdriveSmart27102022.Click(48, 10);
+  if (Sys.Browser("chrome").IsOpen) {
+    Log.Event("Browser was runned");
+  } else {
+    Log.Error("Browser was not runned");
+  }
+  TestedApps.PDDTests.Close();
+}
+
+function Test5()
+{
+  if (Sys.Browser("chrome").IsOpen) Sys.Browser("chrome").Terminate();
+  TestedApps.PDDTests.Run();
+    //Clicks the 'AmButton4' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.AmButton4.Click(78, 5);
+  if (Sys.Browser("chrome").IsOpen) {
+    Log.Event("Browser was runned");
+  } else {
+    Log.Error("Browser was not runned");
+  }
+  TestedApps.PDDTests.Close();
+}
+
+function Test6()
+{
+  TestedApps.PDDTests.Run();
+  Project.Variables.LoopVar.Reset();
+  for(; !Project.Variables.LoopVar.IsEOF();)
+  {
+      //Clicks the 'AmButton3' object.
+  Sys.Process("PDDTests").WPFObject("HwndSource: MainWindow", "ADrive ????? 2022 ?? ???????? ????????? ????????").WPFObject("MainWindow", "ADrive ????? 2022 ?? ???????? ????????? ????????", 1).WPFObject("SubjectMenuView", "", 1).WPFObject("Grid", "", 1).WPFObject("Grid", "", 3).WPFObject("DockPanel", "", 1).WPFObject("ItemsControl", "", 1).WPFObject("ContentPresenter", "", 5).Click();
+  //Clicks the 'AmButton' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.AmButton.Click();
+  //Clicks the 'Button3' button.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.ItemsControl.Button3.ClickButton();
+  //Clicks the 'AmButton2' object.
+  Aliases.PDDTests.HwndSource_MainWindow.MainWindow.AmButton2.Click(104, 4);
+    //Enters KeywordTests.Test6.Variables.LoopVar(1) in the 'MainWindow' object.
+    Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys(Project.Variables.LoopVar.Value(1));
+    //Enters KeywordTests.Test6.Variables.LoopVar(2) in the 'MainWindow' object.
+    Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys(Project.Variables.LoopVar.Value(2));
+    //Enters KeywordTests.Test6.Variables.LoopVar(3) in the 'MainWindow' object.
+    Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys(Project.Variables.LoopVar.Value(3));
+    //Enters KeywordTests.Test6.Variables.LoopVar(4) in the 'MainWindow' object.
+    Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys(Project.Variables.LoopVar.Value(4));
+    //Enters KeywordTests.Test6.Variables.LoopVar(5) in the 'MainWindow' object.
+    Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys(Project.Variables.LoopVar.Value(5));
+    //Enters KeywordTests.Test6.Variables.LoopVar(6) in the 'MainWindow' object.
+    Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys(Project.Variables.LoopVar.Value(6));
+    //Enters KeywordTests.Test6.Variables.LoopVar(7) in the 'MainWindow' object.
+    Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys(Project.Variables.LoopVar.Value(7));
+    //Enters KeywordTests.Test6.Variables.LoopVar(8) in the 'MainWindow' object.
+    Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys(Project.Variables.LoopVar.Value(8));
+    //Enters KeywordTests.Test6.Variables.LoopVar(9) in the 'MainWindow' object.
+    Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys(Project.Variables.LoopVar.Value(9));
+    //Enters KeywordTests.Test6.Variables.LoopVar(10) in the 'MainWindow' object.
+    Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys(Project.Variables.LoopVar.Value(10));
+    //Enters KeywordTests.Test6.Variables.LoopVar(11) in the 'MainWindow' object.
+    Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys(Project.Variables.LoopVar.Value(11));
+    //Enters KeywordTests.Test6.Variables.LoopVar(12) in the 'MainWindow' object.
+    Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys(Project.Variables.LoopVar.Value(12));
+    //Enters KeywordTests.Test6.Variables.LoopVar(13) in the 'MainWindow' object.
+    Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys(Project.Variables.LoopVar.Value(13));
+    //Enters KeywordTests.Test6.Variables.LoopVar(14) in the 'MainWindow' object.
+    Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys(Project.Variables.LoopVar.Value(14));
+    //Enters KeywordTests.Test6.Variables.LoopVar(15) in the 'MainWindow' object.
+    Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys(Project.Variables.LoopVar.Value(15));
+    //Enters KeywordTests.Test6.Variables.LoopVar(16) in the 'MainWindow' object.
+    Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys(Project.Variables.LoopVar.Value(16));
+    //Enters KeywordTests.Test6.Variables.LoopVar(17) in the 'MainWindow' object.
+    Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys(Project.Variables.LoopVar.Value(17));
+    //Enters KeywordTests.Test6.Variables.LoopVar(18) in the 'MainWindow' object.
+    Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys(Project.Variables.LoopVar.Value(18));
+    //Enters KeywordTests.Test6.Variables.LoopVar(19) in the 'MainWindow' object.
+    Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys(Project.Variables.LoopVar.Value(19));
+    //Enters KeywordTests.Test6.Variables.LoopVar(20) in the 'MainWindow' object.
+    Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys(Project.Variables.LoopVar.Value(20));
+    //Enters KeywordTests.Test6.Variables.LoopVar(21) in the 'MainWindow' object.
+    Aliases.PDDTests.HwndSource_MainWindow.MainWindow.Keys(Project.Variables.LoopVar.Value(21));
+    Project.Variables.LoopVar.Next();
+    //Clicks the 'AmButton3' object.
+    Sys.Process("PDDTests").WPFObject("HwndSource: MainWindow", "ADrive ????? 2022 ?? ???????? ????????? ????????").WPFObject("MainWindow", "ADrive ????? 2022 ?? ???????? ????????? ????????", 1).WPFObject("SubjectMenuView", "", 1).WPFObject("Grid", "", 1).WPFObject("Grid", "", 3).WPFObject("DockPanel", "", 1).WPFObject("ItemsControl", "", 1).WPFObject("ContentPresenter", "", 1).Click();
+  }
+  TestedApps.PDDTests.Close();
+}
